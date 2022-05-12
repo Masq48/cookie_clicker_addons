0565b217ab5e2008f05ddc26dd9e0b29110855d9
Masq48 <99148513+Masq48@users.noreply.github.com>
2022-05-12 11:06:42 -0700

    Fixed again

--- addon.js
+++ addon.js
@@ -1,5 +1,5 @@
 Game.registerMod("Test mod", {
     init:function() {
-        Game.registerHook("click",function() {Game.Notify("Hello");});
+        Game.registerHook('click',function(){Game.Notify(choose(['A good click.','A solid click.','A mediocre click.','An excellent click!']),'',0,0.5);});
     }
 });
